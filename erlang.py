f888e82081c6073f1f12a2402f579a5ebdc77a2c
dawid31 <dawidkedzierski04@gmail.com>
2023-04-19 23:07:55 +0200

    implemented func that calc the number of lines gui update you choose calc mode from drop-down menu what u want to calculate

--- erlang.py
+++ erlang.py
@@ -1,5 +1,4 @@
 import math
-
 '''
 Wzór dla modelu Erlanga służącego do wyznaczania współczynnika blokady w systemach z jednym kanałem
 (czyli jedną linią obsługi) to:
@@ -13,15 +12,51 @@ A to intensywność przychodzących zgłoszeń (liczba zgłoszeń na jednostkę 
 N to liczba kanałów (linii obsługi),
 sum(k=0 to N) (A^k / k!) to suma prawdopodobieństw, że kanały będą zajęte.
 '''
+#calculates value of a^n / n! for big nums to avoid overflow
+def ann(n, a):
+        """
+        :param n: lines
+        :param a: traffic
+        :return: a^n / n!
+        """
+        result = 1
+        for n in range(1, n + 1):
+            result = result * a / n
 
-def erlang_b(A, N):
-    numerator  = (A**N) / math.factorial(N)
-    denominator = 0
+        return result     
+     
 
-    for k in range(N+1):
-        denominator += (A**k)/math.factorial(k)
+def calculate_erlang_b(traffic, lines):
+        tab = [x for x in range(lines + 1)]
+        sum = 0
+        for idx, val in enumerate(tab):
+            sum += ann(idx, traffic)
 
-    B = numerator/denominator
-    return B
+        blocking_rate = ann(lines, traffic) / sum
 
-erlang_value = erlang_b(100, 10)
+        return blocking_rate
+
+
+def calculate_erlang_n(traffic, blocking_rate):
+        """
+        calculate the number of lines in a trunk group // oblicza ilosc linii
+        :param traffic: the traffic in Erlangs // natezenie ruchu w erlangach
+        :param blocking_rate: blocking rate / wspolczynnik blokady
+        :return: number of lines / ilosc linii
+        """
+        lines = 1
+        blocking_rate_searched = blocking_rate
+        while blocking_rate_searched >= blocking_rate:
+            lines += 1
+            blocking_rate_searched = calculate_erlang_b(traffic, lines)
+
+        return lines-1
+
+
+def calculate_erlang_a(lines, blocking_rate):
+    return "work in progress"
+
+print(calculate_erlang_b(100, 10))
+#print(calculate_erlang_n(100, 0.901074))
+#print(calculate_erlang_a(123, 123))
+
--- gui.py
+++ gui.py
@@ -6,19 +6,36 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from erlang import erlang_b
+from kivy.uix.spinner import Spinner
+from erlang import calculate_erlang_a, calculate_erlang_b, calculate_erlang_n
 
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
-
         self.cols = 2
 
-        self.add_widget(Label(text="Intensywność zgłoszeń (w erlangach):"))
+        # Create the input fields and labels
+        self.mode_label = Label(text="Tryb:")
+        self.add_widget(self.mode_label)
+
+        self.mode_spinner = Spinner(
+            text="Wybierz co chcesz policzyć?",
+            values=["Natężenie ruchu work in progress :)", "Współczynnik blokady", "Ilość linii/kanałów"],
+            size_hint=(None, None),
+            size=(250, 44)
+        )
+        self.mode_spinner.bind(text=self.update_input_labels)
+        self.add_widget(self.mode_spinner)
+
+        self.intensity_label = Label(text="Intensywność zgłoszeń (w erlangach):")
+        self.add_widget(self.intensity_label)
+
         self.intensity = TextInput(multiline=False)
         self.add_widget(self.intensity)
 
-        self.add_widget(Label(text="Liczba kanałów:"))
+        self.channels_label = Label(text="Liczba kanałów:")
+        self.add_widget(self.channels_label)
+
         self.channels = TextInput(multiline=False)
         self.add_widget(self.channels)
 
@@ -29,12 +46,40 @@ class MyGridLayout(GridLayout):
         self.result_label = Label(text="Tu pojawi się wynik")
         self.add_widget(self.result_label)
 
+
+    def update_input_labels(self, instance, value):
+        # Update the labels of the input fields depending on the selected mode
+        if value == "Natężenie ruchu work in progress :)":
+            self.intensity_label.text = "Ilość linii:"
+            self.channels_label.text = "Współczynnik blokady:"
+        elif value == "Współczynnik blokady":
+            self.intensity_label.text = "Intensywność zgłoszeń (w erlangach):"
+            self.channels_label.text = "Liczba kanałów/linii:"
+        elif value == "Ilość linii/kanałów":
+            self.intensity_label.text = "Intensywność zgłoszeń (w erlangach)"
+            self.channels_label.text = "Współczynnik blokady:"
+    
     def calculate(self, instance):
         # Define the function that will be called when the Run button is pressed
-        intensity = int(self.intensity.text)
-        channels = int(self.channels.text)
-        result = erlang_b(intensity, channels)
-        self.result_label.text = str(round(result, 5))
+        mode = self.mode_spinner.text
+        if mode == 'Natężenie ruchu work in progress :)':
+            #field1 = int(self.intensity.text)
+            #field2 = float(self.channels.text)
+            #result = calculate_erlang_a(field1, field2) doesnt do anything yet
+            self.result_label.text = "W trakcie implementacji"
+
+        elif mode == 'Współczynnik blokady':
+            field1 = float(self.intensity.text)
+            field2 = int(self.channels.text)
+            result = calculate_erlang_b(field1, field2) 
+            self.result_label.text = str(round(float(result), 5))
+
+
+        elif mode == 'Ilość linii/kanałów':
+            field1 = float(self.intensity.text)
+            field2 = float(self.channels.text)
+            result = calculate_erlang_n(field1, field2)
+            self.result_label.text = str(round(float(result), 5))
 
 class ErlangCalculator(App):
     def build(self):
