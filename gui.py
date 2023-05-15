6ed3c8bff41770954db9719656abbe1fa488478a
dawid31 <dawidkedzierski04@gmail.com>
2023-05-15 20:08:21 +0200

    added instruction to gui

--- gui.py
+++ gui.py
@@ -8,13 +8,19 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 from erlang import calculate_erlang_a, calculate_erlang_b, calculate_erlang_n
+from functools import partial
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.image import Image
+
 
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
-        self.cols = 2
+        self.cols = 1
+        self.orientation = 'tb-lr'
 
-        # Create the input fields and labels
+
         self.mode_label = Label(text="Tryb:")
         self.add_widget(self.mode_label)
 
@@ -27,16 +33,19 @@ class MyGridLayout(GridLayout):
         self.mode_spinner.bind(text=self.update_input_labels)
         self.add_widget(self.mode_spinner)
 
-        self.intensity_label = Label(text="Intensywność zgłoszeń (w erlangach):")
+        self.intensity_label = Label(text="Intensywność zgłoszeń (w erlangach):", font_size=40)
         self.add_widget(self.intensity_label)
 
-        self.intensity = TextInput(multiline=False)
+        # self.image = Image(source="arrow-down.png")
+        # self.add_widget(self.image)
+
+        self.intensity = TextInput(multiline=False, size_hint=(0.3, None), height=40)
         self.add_widget(self.intensity)
 
-        self.channels_label = Label(text="Liczba kanałów:")
+        self.channels_label = Label(text="Liczba kanałów:", font_size=40)
         self.add_widget(self.channels_label)
 
-        self.channels = TextInput(multiline=False)
+        self.channels = TextInput(multiline=False, size_hint=(0.3, None), height=40)
         self.add_widget(self.channels)
 
         self.submit = Button(text="Oblicz", font_size=40)
@@ -46,6 +55,33 @@ class MyGridLayout(GridLayout):
         self.result_label = Label(text="Tu pojawi się wynik")
         self.add_widget(self.result_label)
 
+
+        box_layout = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
+
+        # Create the "?" button
+        button = Button(text="?", size_hint=(None, None), size=(50, 50))
+        button.bind(on_press=partial(self.show_help_popup, "xd"))
+        box_layout.add_widget(button)
+
+        # Add the box layout to the main layout
+        self.add_widget(box_layout)
+
+    def show_help_popup(self, label_text, instance):
+        # Define the help messages for each label
+        help_messages = {
+            "Tryb:": "Wybierz co chcesz policzyć?",
+            "Co powinno być w tym polu?": "Podaj intensywność zgłoszeń w erlangach.",
+            "Liczba kanałów:": "Podaj liczbę kanałów."
+        }
+
+        # Retrieve the help message based on the label text
+        help_message = "tutaj bedzie zawartosc insturkcji kiedys :D"
+
+        # Create and open a popup window with the help message
+        popup_content = BoxLayout(orientation='vertical', padding=10)
+        popup_content.add_widget(Label(text=help_message))
+        popup = Popup(title="Pomoc", content=popup_content, size_hint=(None, None), size=(400, 200))
+        popup.open()
 
     def update_input_labels(self, instance, value):
         # Update the labels of the input fields depending on the selected mode
