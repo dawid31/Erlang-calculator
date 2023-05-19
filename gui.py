3be78570b03c641133eff9a917566e90ec4e4ea3
dawid31 <dawidkedzierski04@gmail.com>
2023-05-19 23:41:17 +0200

    adding sliders widget to select interval

--- gui.py
+++ gui.py
@@ -13,6 +13,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from instruction import instruction_text
+from kivy.uix.slider import Slider
 
 
 class MainScreen(Screen):
@@ -49,13 +50,12 @@ class MainScreen(Screen):
         self.layout.add_widget(self.result_label)
 
         box_layout = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
+        self.layout.add_widget(box_layout)
 
         button = Button(text="Go to Second Screen", size_hint=(None, None), size=(200, 50))
         button.bind(on_press=self.goto_second_screen)
+
         box_layout.add_widget(button)
-
-        self.layout.add_widget(box_layout)
-
         self.add_widget(self.layout)
 
     def goto_second_screen(self, instance):
@@ -72,6 +72,14 @@ class MainScreen(Screen):
             self.intensity_label.text = "Intensywność zgłoszeń (w erlangach)"
             self.channels_label.text = "Współczynnik blokady:"
 
+    def display_value_error(self):
+        value_error_message = '''Podano nieprawidlowe wartosci w polu/polach.
+        Sprawdz w instrukcji w lewym dolnym rogu co poszlo nie tak.'''
+        popup_content = BoxLayout(orientation='vertical', padding=10)
+        popup_content.add_widget(Label(text=value_error_message))
+        popup = Popup(title="Podano nieprawidlowe wartosci", content=popup_content, size_hint=(None, None), size=(600, 300))
+        popup.open()
+
 
     def calculate(self, instance):
         # Define the function that will be called when the Run button is pressed
@@ -81,45 +89,27 @@ class MainScreen(Screen):
                 field1 = int(self.intensity.text)
                 field2 = float(self.channels.text)
                 result = calculate_erlang_a(field1, field2) 
-                self.result_label.text = str(round(float(result), 5))
+                self.result_label.text = f"{mode}: {str(round(float(result), 5))}"
             except ValueError:
-                value_error_message = '''Podano nieprawidlowe wartosci w polu/polach.
-Sprawdz w instrukcji w lewym dolnym rogu co poszlo nie tak.'''
-                popup_content = BoxLayout(orientation='vertical', padding=10)
-                popup_content.add_widget(Label(text=value_error_message))
-                popup = Popup(title="Podano nieprawidlowe wartosci", content=popup_content, size_hint=(None, None), size=(600, 300))
-                popup.open()
+                self.display_value_error()
 
         elif mode == 'Współczynnik blokady':
             try:
-                if "," in self.intensity.text:
-                    self.intensity.text.replace(",", ".") #not working properly
                 field1 = float(self.intensity.text)
                 field2 = int(self.channels.text)
                 result = calculate_erlang_b(field1, field2) 
-                self.result_label.text = str(round(float(result), 5))
+                self.result_label.text = f"{mode}: {str(round(float(result), 5))}"
             except ValueError:
-                value_error_message = '''Podano nieprawidlowe wartosci w polu/polach.
-Sprawdz dwie ostatnie linijki w instrukcji w lewym dolnym rogu i upewnij sie, ze w pola wpisane sa prawidlowe wartosci'''
-                popup_content = BoxLayout(orientation='vertical', padding=10)
-                popup_content.add_widget(Label(text=value_error_message))
-                popup = Popup(title="Podano nieprawidlowe wartosci", content=popup_content, size_hint=(None, None), size=(600, 300))
-                popup.open()
-
+                self.display_value_error()
 
         elif mode == 'Ilość linii/kanałów':
             try:
                 field1 = float(self.intensity.text)
                 field2 = float(self.channels.text)
                 result = calculate_erlang_n(field1, field2)
-                self.result_label.text = str(round(float(result), 5))
+                self.result_label.text = f"{mode}: {str(round(float(result), 5))}"
             except ValueError:
-                value_error_message = '''Podano nieprawidlowe wartosci w polu/polach.
-Sprawdz w instrukcji w lewym dolnym rogu co poszlo nie tak.'''
-                popup_content = BoxLayout(orientation='vertical', padding=10)
-                popup_content.add_widget(Label(text=value_error_message))
-                popup = Popup(title="Podano nieprawidlowe wartosci", content=popup_content, size_hint=(None, None), size=(600, 300))
-                popup.open()
+                self.display_value_error()
 
 
 class SecondScreen(Screen):
@@ -127,22 +117,46 @@ class SecondScreen(Screen):
         super(SecondScreen, self).__init__(**kwargs)
         self.layout = GridLayout(cols=1, orientation='tb-lr')
 
-        self.label = Label(text="This is the Second Screen", font_size=40)
-        self.layout.add_widget(self.label)
+        # Slider and Field Layout
+        slider_layout = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
+        
+        # Slider
+        self.slider = Slider(min=0, max=100, value=50, step=1)
+        self.slider.bind(value=self.on_slider_value_change)
+        slider_layout.add_widget(self.slider)
 
-        box_layout = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
+        # Field displaying slider value
+        self.value_field = TextInput(text=str(self.slider.value), multiline=False, size_hint=(None, None), width=100, height=50)
+        self.value_field.bind(text=self.on_value_field_text_change)
+        slider_layout.add_widget(self.value_field)
 
+        self.layout.add_widget(slider_layout)
+
+        self.graph_button = Button(text="Oblicz", font_size=40)
+        slider_layout.add_widget(self.graph_button)
+
+
+        # Going back to the main screen part of GUI
+        box_layout2 = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
         button = Button(text="Go to Main Screen", size_hint=(None, None), size=(200, 50))
         button.bind(on_press=self.goto_main_screen)
-        box_layout.add_widget(button)
-
-        self.layout.add_widget(box_layout)
-
+        box_layout2.add_widget(button)
+        self.layout.add_widget(box_layout2)
         self.add_widget(self.layout)
+
+    def on_slider_value_change(self, instance, value):
+        self.value_field.text = str(value)
+
+    def on_value_field_text_change(self, instance, text):
+        try:
+            value = float(text)
+            if self.slider.min <= value <= self.slider.max:
+                self.slider.value = value
+        except ValueError:
+            pass
 
     def goto_main_screen(self, instance):
         self.manager.current = 'main'
-
 
 class ErlangCalculatorApp(App):
     def build(self):
